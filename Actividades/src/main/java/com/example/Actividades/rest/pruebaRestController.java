ba3330d110e879cbc17abbe7f5670772f108203c
Youssef <220238@fppiramide.com>
2023-09-25 10:44:17 +0200

    .

--- Actividades/src/main/java/com/example/Actividades/rest/pruebaRestController.java
+++ Actividades/src/main/java/com/example/Actividades/rest/pruebaRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class pruebaRestController {
-    @GetMapping("/")
+    @GetMapping("/")//
     String holaMundo(){
         return "Hola Mundo!";
     }
